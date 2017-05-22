c6af53b42e3805808e66c44af73dea42409578af
Marin Leontenko <marin.leontenko@gmail.com>
2017-05-22 12:45:49 +0200

    Add backend component for changing value of "processed" for selected obj

--- parse_file.php
+++ parse_file.php
@@ -3,6 +3,11 @@
 $dbconn = pg_connect("host=localhost dbname=borders user=admin password=admin")
   or die('Could not connect: ' . pg_last_error());
 
-var_dump($_POST['featureId']);
+
 $featureID = $_POST['featureId'];
+
+//Performing SQL query
+$query = "UPDATE borders SET processed = 1 WHERE borders.gid =".$featureID;
+$result = pg_query($query) or die ('Querry failed: ' . pg_last_error());
+
 ?>
