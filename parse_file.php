b987bc76f5e8f8c20fa169f8c88c4e32a5812b42
Marin Leontenko <marin.leontenko@gmail.com>
2017-05-22 09:32:36 +0200

    Add database connection parameters

--- parse_file.php
+++ parse_file.php
@@ -1,3 +1,8 @@
 <?php
+//Connecting, selecting database
+$dbconn = pg_connect("host=localhost dbname=borders user=admin password=admin")
+  or die('Could not connect: ' . pg_last_error());
+
 var_dump($_POST['featureId']);
+$featureID = $_POST['featureId'];
 ?>
