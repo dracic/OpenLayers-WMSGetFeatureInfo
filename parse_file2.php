90413ec4c7c55dfe19425d28ab390b8937a15dd6
Marin Leontenko <marin.leontenko@gmail.com>
2017-06-01 15:05:20 +0200

    Change wms layer and database to tk25gridQC

--- parse_file2.php
+++ parse_file2.php
@@ -1,13 +1,13 @@
 <?php
 //Connecting, selecting database
-$dbconn = pg_connect("host=localhost dbname=borders user=admin password=admin")
+$dbconn = pg_connect("host=vm1006.exovps.hr dbname=egn user=egn password=egn")
   or die('Could not connect: ' . pg_last_error());
 
 
 $featureID = $_POST['idd'];
 
 //Performing SQL query
-$query = "UPDATE borders SET processed = 0 WHERE borders.gid =".$featureID;
+$query = "UPDATE tk25_grid_qc SET qc = 0 WHERE tk25_grid_qc.gid =".$featureID;
 $result = pg_query($query) or die ('Querry failed: ' . pg_last_error());
 
 ?>
